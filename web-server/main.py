f4b03940ec19818c623eb637f1c4bc7bdefc1b9d
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2023-12-02 19:26:45 -0300

    clase 15

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,34 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3]
+
+@app.get('/contact')
+def get_list():
+    return {'name': 'Platzi'}
+
+@app.get('/contacto', response_class=HTMLResponse)
+def get_list():
+    return  """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+            <p>Soy un párrafo</p>
+        </body>
+    </html>
+    """
+
 
 def run():
     store.get_categories()
-    
+
 if __name__ == '__main__':
     run()
